63227ccca963822c42453a3a5e9ffe858777cf13
abysswalker0000 <abysswalker0000@mail.ru>
2025-02-01 19:18:12 +0300

    Design + half func

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.5")
+    implementation("org.mozilla:rhino:1.7R4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/androidcalculator/Calculator.kt
+++ app/src/main/java/com/example/androidcalculator/Calculator.kt
@@ -1,4 +1,6 @@
 package com.example.androidcalculator
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,11 +11,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +28,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
+val openSans = FontFamily(
+    Font(R.font.opsmed, FontWeight.W400),
+    Font(R.font.opsbld, FontWeight.W900),
+)
 
 
 
@@ -36,37 +47,56 @@ val buttonList = listOf(
 
 )
 @Composable
-fun Calculator(modifier: Modifier = Modifier)
-{
-    Box(modifier = modifier) {
+fun Calculator(modifier: Modifier = Modifier, viewModel: CalculatorViewModel)
+{   val equationText=viewModel.eqationText.observeAsState()
+    val resultText=viewModel.resultText.observeAsState()
+    Box(modifier = modifier.background(color = Color(0xFF262626)).padding(bottom = 2.dp)) {
         Column(modifier=modifier.fillMaxSize(),
-            horizontalAlignment =Alignment.Start)
+            horizontalAlignment =Alignment.Start,
+            )
             {
-            Text(text="12341+1312",
+            Text(text=equationText.value?:"",
+                modifier= Modifier.padding(start = 16.dp),
                 style = TextStyle(
-                fontSize = 30.sp,
+                fontSize = 35.sp,
+                    color = Color(0xFFD0BC2C),
                 textAlign = TextAlign.Start,
                 ),
-                maxLines = 4,
+                maxLines = 3,
                 overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.W900,
+                fontFamily = openSans,
+
                 )
 
                 Spacer(modifier=Modifier.weight(1f))
 
-                Text(text="246",
+                Text(text=resultText.value?:"0",
+                    modifier= Modifier.padding(start = 16.dp),
                     style = TextStyle(
-                        fontSize = 50.sp,
-                        textAlign = TextAlign.Start
+                        fontSize = 25.sp,
+                        textAlign = TextAlign.Start,
+                        color = Color(0xFFD0BC2C),
+                        fontWeight = FontWeight.W900,
+                        fontFamily = openSans,
                     ),
                     maxLines = 2,)
 
                 Spacer(modifier=Modifier.height(10.dp))
 
 
-                LazyVerticalGrid(columns = GridCells.Fixed(4))
+                LazyVerticalGrid(columns = GridCells.Fixed(4),
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement =  Arrangement.spacedBy(5.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    )
+
+
                 {
                     items(buttonList){
-                        CalculatorButton(btn=it)
+                        CalculatorButton(btn=it, onClick = {
+                            viewModel.onButtonClick(it)
+                        })
                     }
             }
         }
@@ -74,21 +104,29 @@ fun Calculator(modifier: Modifier = Modifier)
 }
 
 @Composable
-fun CalculatorButton(btn : String) {
-    Box(modifier = Modifier.padding(10.dp)){
-        FloatingActionButton(onClick = {},
-            modifier = Modifier.size(60.dp),
-            contentColor = Color.Yellow,
+fun CalculatorButton(btn : String, onClick : () -> Unit) {
+    Box(modifier = Modifier.padding(3.dp)){
+        FloatingActionButton(onClick = onClick,
+            modifier = Modifier.size(65.dp),
+            contentColor = getContentColor(btn),
             containerColor= getButtonColor(btn),
+            shape = CircleShape
         )
         {
-            Text(text=btn, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Text(text=btn, fontSize = 20.sp, fontWeight = FontWeight.W400, fontFamily = openSans)
         }
     }
 }
 
 fun getButtonColor(btn : String) : Color{
     if (btn == "1" || btn == "2" || btn == "3" || btn == "4" ||btn == "5" ||btn == "6" ||btn == "7" ||btn == "8" ||btn == "9" ||btn == "0")
-        return Color(0xFF595959)
-    else return Color.DarkGray
+        return Color(0xFF13120F)
+    if( btn=="=" || btn =="C")
+        return Color(0xFFD0BC2C)
+    else return Color(0xFF00000F)
+}
+fun getContentColor(btn : String) : Color{
+    if (btn=="=" || btn=="C")
+        return Color(0xFF00000F)
+    else return Color(0xFFD0BC2C)
 }
